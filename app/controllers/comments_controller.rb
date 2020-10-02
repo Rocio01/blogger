92aba1512cbbe123f25300d912035d341364404e
mohamedsabry3023@gmail.com <you@example.com>
2020-10-02 17:16:40 +0200

    add time stamp for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.article_id = params[:article_id]
+        @comment.save 
+        flash.notice = "'#{@comment.author_name}\'s comment has been Created!'"
+        redirect_to article_path(@comment.article)
+    end
+    def comment_params
+        params.require(:comment).permit(:author_name, :body)
+    end
+end
