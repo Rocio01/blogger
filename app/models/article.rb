baf570f77c7d016f3eb266007e64907108ce877d
Rocio01 <zrmartinezg@gmail.com>
2020-10-02 17:54:15 -0500

    Add rubocop file

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :taggings, dependent: :destroy
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Tag < ApplicationRecord
   has_many :taggings, dependent: :destroy
   has_many :articles, through: :taggings
